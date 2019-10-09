0f0b36eba5abfe886ddfd9822d619dcc7581f761
沈斌 <bin.shen@ailingua.com.cn>
2019-10-09 15:08:43 +0800

    使用druid替换hikariCP

---
+++ giant-cloud-service/giant-cloud-druid/src/main/java/com/giant/cloud/druid/GiantDruidApplication.java
@@ -0,0 +1,12 @@
+package com.giant.cloud.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiantDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiantDruidApplication.class, args);
+    }
+}
